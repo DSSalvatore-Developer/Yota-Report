db65b594d1e6f835ead0eef17f10d31ceef7926f
DSSalvatore-Developer <D.S.Salvatore@iCloud.com>
2025-04-27 13:46:16 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,28 @@
-// MARK: Блок - Конфигурация Firebase
+// MARK: Блок - Тестовая конфигурация Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCl5ia_vBLQ-VZziIyiGtJsBng4Z2SUW8I",
-  authDomain: "yota-report-4a391.firebaseapp.com",
-  databaseURL: "https://yota-report-4a391-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "yota-report-4a391",
-  storageBucket: "yota-report-4a391.firebasestorage.app",
-  messagingSenderId: "401712839981",
-  appId: "1:401712839981:web:65474e9ca8bf5673a5e7a4"
+    apiKey: "AIzaSyC3fFakeTestKEY123456789",
+    authDomain: "yota-report-test.firebaseapp.com",
+    databaseURL: "https://yota-report-test-default-rtdb.firebaseio.com",
+    projectId: "yota-report-test",
+    storageBucket: "yota-report-test.appspot.com",
+    messagingSenderId: "123456789000",
+    appId: "1:123456789000:web:1234567890abcdef"
 };
 
-// MARK: Блок - Инициализация Firebase
-firebase.initializeApp(firebaseConfig);
+// MARK: Блок - Инициализация Firebase с обработкой ошибок
+try {
+    firebase.initializeApp(firebaseConfig);
+} catch (error) {
+    console.error("Ошибка инициализации Firebase:", error);
+    showError("Не удалось подключиться к базе данных. Попробуйте позже.");
+}
+
+// MARK: Блок - Функция показа ошибки пользователю
+function showError(message) {
+    document.body.innerHTML = `
+        <div style="text-align: center; margin-top: 50px;">
+            <h1 style="color: #0077c8;">Ошибка загрузки</h1>
+            <p>${message}</p>
+        </div>
+    `;
+}
